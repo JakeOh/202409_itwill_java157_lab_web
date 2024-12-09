2a307a2e173dd916f6395c6bd3bf22dd6b4d42ed
JakeOh <jake.oh.lecture@gmail.com>
2024-12-09 15:09:04 +0900

    JS for loop

--- lab03_js/src/main/webapp/js/for.js
+++ lab03_js/src/main/webapp/js/for.js
@@ -16,5 +16,24 @@ result.innerHTML = html;
 result.innerHTML += '<hr />'; // result.innerHTML = result.innerHTML + '<hr />'
 
 // 구구단 3단 ~ 9단까지를 result에 추가: result.innerHTML += 문자열;
+html = '';
+for (let x = 3; x < 10; x++) {
+    for (let y = 1; y < 10; y++) {
+        html += `${x} x ${y} = ${x * y} <br />`;
+    }
+    html += '<hr />';
+}
+result.innerHTML += html;
 
-
+// break를 사용해서 2단은 2x2까지, 3단은 3x3까지, ..., 9단은 9x9까지 result에 추가.
+html = '';
+for (let x = 2; x < 10; x++) {
+    for (let y = 1; y < 10; y++) {
+        html += `${x} x ${y} = ${x * y} <br />`;
+        if (x === y) {
+            break;
+        }
+    }
+    html += '<hr />';
+}
+result.innerHTML += html;
