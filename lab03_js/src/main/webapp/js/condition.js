69ddec7383ca3775e46ec9a71a77a7c866224f3e
JakeOh <jake.oh.lecture@gmail.com>
2024-12-09 10:24:55 +0900

    JS conditional statement

---
+++ lab03_js/src/main/webapp/js/condition.js
@@ -0,0 +1,11 @@
+/**
+ * condition.html 파일에 포함.
+ */
+
+// 아이디가 numberInput인 input 요소를 찾음.
+const numberInput = document.querySelector('input#numberInput');
+console.log(numberInput);
+
+// 아이디가 result인 div 요소를 찾음.
+const result = document.querySelector('div#result');
+console.log(result);
