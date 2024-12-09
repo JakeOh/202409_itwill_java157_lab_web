d63f9b6bfdc544bb1d0e0728e8f08b806e686ade
JakeOh <jake.oh.lecture@gmail.com>
2024-12-09 16:25:54 +0900

    JS while

--- lab03_js/src/main/webapp/js/while.js
+++ lab03_js/src/main/webapp/js/while.js
@@ -15,3 +15,19 @@ while (n <= 5) {
 }
 list.innerHTML = html;
 
+// 아이디가 tableBody인 tbody 요소를 찾음.
+const tableBody = document.querySelector('tbody#tableBody');
+
+// while 반복문을 사용해서 tr 5개를 추가.
+html = '';
+n = 1;
+while (n <= 5) {
+    html += `
+    <tr>
+        <td>${n}</td>
+        <td>제목 ${n}</td>
+    </tr>
+    `;
+    n++;
+}
+tableBody.innerHTML = html;
