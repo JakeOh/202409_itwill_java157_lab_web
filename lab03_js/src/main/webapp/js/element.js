0b24b57b58005f103ea26bf2a6aa33aac73403e1
JakeOh <jake.oh.lecture@gmail.com>
2024-12-11 17:17:50 +0900

    get HTML elements

---
+++ lab03_js/src/main/webapp/js/element.js
@@ -0,0 +1,10 @@
+/**
+ * element.html 파일에 포함.
+ * 
+ * HTML 요소를 찾는 메서드:
+ * - document.getElementById
+ * - document.getElementsByClassName
+ * - document.getElementsByTagName
+ * - document.querySelector
+ * - document.querySelectorAll
+ */
