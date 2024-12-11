23e485d95ed460ee451035205d4d718d9a5e32b0
JakeOh <jake.oh.lecture@gmail.com>
2024-12-11 16:25:14 +0900

    object destructuring assignment

---
+++ lab03_js/src/main/webapp/js/destructure.js
@@ -0,0 +1,49 @@
+/**
+ * destructure.html 파일에 포함.
+ */
+
+function println1(student) {
+    console.log(`번호: ${student.no}, 이름: ${student.name}, 학년: ${student.grade}, 반: ${student.classNo}`);
+}
+
+function println2(student) {
+    const no = student.no;
+    const name = student.name;
+    const grade = student.grade;
+    const classNo = student.classNo;
+    
+    console.log(`번호: ${no}, 이름: ${name}, 학년: ${grade}, 반: ${classNo}`);
+}
+
+function println3({ no, name, grade, classNo }) {
+    console.log(`번호: ${no}, 이름: ${name}, 학년: ${grade}, 반: ${classNo}`);
+}
+
+const student1 = {
+    no: 100,
+    name: '오쌤',
+    grade: 1,
+    classNo: 2,
+};
+
+println1(student1);
+println2(student1);
+println3(student1);
+
+const student2 = {
+    no: 200,
+    name: '홍길동',
+};
+
+println1(student2);
+println2(student2);
+println3(student2);
+
+function printStudent({ name, ...rest }) {
+    console.log(`name = ${name}`);
+    console.log(rest);
+}
+
+printStudent(student1);
+printStudent(student2);
+printStudent({ name: '아이티', no: 123, email: 'it@itwill.com' });
