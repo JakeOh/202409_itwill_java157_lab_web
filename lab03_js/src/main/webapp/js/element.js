1e2eca06084a65d1c8b72c463580835725a4509f
JakeOh <jake.oh.lecture@gmail.com>
2024-12-12 11:24:57 +0900

    document.getElementsByTagName, document.querySelector, document.querySelectorAll

--- lab03_js/src/main/webapp/js/element.js
+++ lab03_js/src/main/webapp/js/element.js
@@ -36,3 +36,41 @@ btn2.addEventListener('click', function () {
         e.style.backgroundColor = 'tomato'; 
     }
 });
+
+// button#btn3 요소를 찾음.
+const btn3 = document.querySelector('button#btn3');
+
+// btn3 요소에 클릭 이벤트 리스너를 설정 -> 모든 div 요소들을 찾아서 바탕색을 변경.
+btn3.addEventListener('click', function () {
+    const divs = document.getElementsByTagName('div');
+    // console.log(divs); //-> HTMLCollection
+    for (const d of divs) {
+        console.log(d);
+        d.style.backgroundColor = 'LightGray';
+    }
+});
+
+// button#btn4 요소를 찾음.
+const btn4 = document.querySelector('button#btn4');
+
+// btn4 요소에 클릭 이벤트 리스너를 설정.
+btn4.addEventListener('click', () => {
+    // div#div4 요소를 찾음.
+    const div4 = document.querySelector('div#div4'); // document.getElementById('div4');
+    console.log(div4);
+    div4.style.backgroundColor = 'SlateBlue';
+});
+
+// button#btn5 요소를 찾음.
+const btn5 = document.querySelector('button#btn5');
+
+// btn5 요소에 클릭 이벤트 리스너를 설정 -> 클래스 이름이 c2인 요소(들)의 바탕색을 변경.
+btn5.addEventListener('click', () => {
+    const divs = document.querySelectorAll('div.c2');
+    // console.log(divs); //-> NodeList - HTML 요소(노드)들의 배열.
+    
+    // for (const d of divs) {
+    //     d.style.backgroundColor = 'violet';
+    // }
+    divs.forEach(d => d.style.backgroundColor = 'violet');
+});
