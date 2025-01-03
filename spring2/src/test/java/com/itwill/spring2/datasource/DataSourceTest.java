ea6211d62ec0861c91e3d41aa8f0ac938e6819c0
JakeOh <jake.oh.lecture@gmail.com>
2025-01-03 11:29:48 +0900

    spring application context, Spring-JUnit test

---
+++ spring2/src/test/java/com/itwill/spring2/datasource/DataSourceTest.java
@@ -0,0 +1,49 @@
+package com.itwill.spring2.datasource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/application-context.xml" })
+public class DataSourceTest {
+
+	/*
+	 * 의존성 주입(DI: Dependency Injection), 제어의 역전(IoC: Inversion of Control)
+	 * 전통적인 자바 개발 방법에서는 객체를 사용하는 곳에서 객체를 생성하고, 그 기능을 이용함.
+	 * 스프링 프레임워크에서는 스프링 컨테이너가 필요한 객체들을 미리 생성하고 관리하고 있다가
+	 * 객체를 필요로 하는 곳에서는 변수 선언과 애너테이션으로
+	 * 스프링 컨테이너가 관리하는 빈(자바 객체)을 주입받아서 사용.
+	 * application-context.xml 파일: 스프링 컨테이너가 생성/관리하는 빈들을 설정하는 파일.
+	 * 
+	 * 스프링 프레임워크: 모델2 MVC(위임 패턴) 아키텍처를 제공하고, 의존성 주입을 제공하는 프레임워크.
+	 */
+	
+	@Autowired
+	private HikariDataSource ds;
+	
+	@Test
+	public void test() throws SQLException {
+		Assertions.assertNotNull(ds);
+		log.debug("ds={}", ds);
+		
+		Connection conn = ds.getConnection();
+		Assertions.assertNotNull(conn);
+		log.debug("conn={}", conn);
+		
+		conn.close();
+		log.debug("커넥션 객체를 풀에 반환.");
+	}
+	
+}
