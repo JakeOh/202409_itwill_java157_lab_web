fb6ed13b12da3b05fcd4c7d80a2fd838849e9157
JakeOh <jake.oh.lecture@gmail.com>
2025-01-08 16:32:47 +0900

    REST API, ResponseEntity

--- spring2/src/main/java/com/itwill/spring2/service/CommentService.java
+++ spring2/src/main/java/com/itwill/spring2/service/CommentService.java
@@ -3,6 +3,7 @@ package com.itwill.spring2.service;
 import org.springframework.stereotype.Service;
 
 import com.itwill.spring2.domain.Comment;
+import com.itwill.spring2.dto.CommentItemDto;
 import com.itwill.spring2.repository.CommentDao;
 
 import lombok.RequiredArgsConstructor;
@@ -17,12 +18,13 @@ public class CommentService {
 	private final CommentDao commentDao;
 	
 	// 해당 아이디의 댓글 (1개)를 검색하는 서비스
-	public Comment readById(Integer id) {
+	public CommentItemDto readById(Integer id) {
 		log.debug("readById(id={})", id);
 		
+		// 영속성 계층의 메서드를 호출해서 select 쿼리를 실행.
 		Comment comment = commentDao.selectById(id);
 		
-		return comment;
+		return CommentItemDto.fromEntity(comment);
 	}
 
 }
--- spring2/src/main/java/com/itwill/spring2/web/CommentController.java
+++ spring2/src/main/java/com/itwill/spring2/web/CommentController.java
@@ -1,11 +1,12 @@
 package com.itwill.spring2.web;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.itwill.spring2.domain.Comment;
+import com.itwill.spring2.dto.CommentItemDto;
 import com.itwill.spring2.service.CommentService;
 
 import lombok.RequiredArgsConstructor;
@@ -23,19 +24,22 @@ public class CommentController {
 	
 	@GetMapping("/{id}")
 	//-> 요청 주소가 변수처럼 변할 수 있는 값(path variable)일 때, {varName} 형식으로 작성.
-	public Comment getCommentById(@PathVariable Integer id) {
+	public ResponseEntity<CommentItemDto> getCommentById(@PathVariable Integer id) {
 		// @PathVariable: 요청 주소의 일부가 변수처럼 변할 수 있는 값일 때,
         // 디스패쳐 서블릿이 요청 주소를 분석해서 메서드의 아규먼트로 전달해줌.
 		// (1) @PathVariable(name = "id") 처럼 경로 변수(path variable)의 이름을 명시하거나,
         // (2) 메서드의 파라미터 이름을 경로 변수와 동일하게 선언하면 됨.
-		// (Eclipse) Window -> Preferences -> Java Compiler ->
+		// (Eclipse) Window -> Preferences -> Java -> Compiler ->
         // "Store information about method parameters (usable via reflection)" 항목을 체크해야 함.
 		
 		log.debug("getCommentById(id={})", id);
 		
-		Comment comment = commentService.readById(id);
+		CommentItemDto comment = commentService.readById(id);
 		
-		return comment;
+		return ResponseEntity.ok(comment);
+		// ResponseEntity<T>
+		// 서버가 클라이언트로 보내는 데이터와 응답코드를 함께 설정할 수 있는 객체.
+		
 		// REST 컨트롤러 메서드가 자바 객체를 리턴하면
         // jackson-databind 라이브러리가 자바 객체를 JSON 문자열로 변환을 담당하고,
         // JSON 문자열이 클라이언트로 전송(응답)됨.
