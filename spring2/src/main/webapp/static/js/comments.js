7f30baf8e5d4f47cacfff48964b433596ee2d03b
JakeOh <jake.oh.lecture@gmail.com>
2025-01-09 16:30:24 +0900

    Ajax, register comment

--- spring2/src/main/webapp/static/js/comments.js
+++ spring2/src/main/webapp/static/js/comments.js
@@ -31,7 +31,38 @@ document.addEventListener('DOMContentLoaded', () => {
     
     // btnRegisterComment 버튼의 클릭 이벤트 리스너(콜백). 댓글 등록.
     function registerComment() {
-        alert('댓글 등록!');
+        // input#id 요소의 값을 읽음 -> 댓글을 등록할 포스트 아이디
+        const postId = document.querySelector('input#id').value;
+        
+        // input#username 요소의 값을 읽음 -> 댓글 작성자 아이디
+        const username = document.querySelector('input#username').value;
+        
+        // textarea#ctext 요소의 값을 읽음 -> 댓글 내용
+        const ctext = document.querySelector('textarea#ctext').value;
+        
+        // 댓글 내용이 비어 있는 지를 체크.
+        if (ctext === '') {
+            alert('댓글 내용을 입력하세요.');
+            return;
+        }
+        
+        // Ajax 요청으로 보낼 데이터 객체
+//        const data = { postId: postId, username: username, ctext: ctext };
+        const data = { postId, username, ctext };
+//        console.log(data);
+        
+        // 서버로 POST 방식의 Ajax 요청을 보내고 응답을 처리.
+        axios.post('../api/comment', data)
+            .then((response) => {
+                // console.log(response);
+                if (response.data === 1) {
+                    alert('1개 댓글 등록 성공');
+                    document.querySelector('textarea#ctext').value = '';
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
     
 });
