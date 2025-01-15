2c2080bdd3b2d73695dc46b41508623c4f6b57a8
JakeOh <jake.oh.lecture@gmail.com>
2025-01-15 10:30:27 +0900

    authentication filter

---
+++ spring2/src/main/java/com/itwill/spring2/filter/AuthenticationFilter.java
@@ -0,0 +1,76 @@
+package com.itwill.spring2.filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Servlet Filter implementation class AuthenticationFilter
+ */
+@Slf4j
+public class AuthenticationFilter extends HttpFilter {
+    
+	private static final long serialVersionUID = 1L;
+
+	/**
+     * @see HttpFilter#HttpFilter()
+     */
+    public AuthenticationFilter() {}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	@Override
+    public void destroy() {
+		// 필터 객체 소멸될 때 해야할 일(예: 리소스 해제)들을 작성.
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
+			throws IOException, ServletException {
+		// 로그인한 사용자(권한이 있는 사용자)는 필터 체인을 진행.
+		// 로그인하지 않은 사용자(권한이 없는 사용자)는 로그인 페이지로 이동(redirect).
+
+		HttpServletRequest httpReq = (HttpServletRequest) request;
+		HttpServletResponse httpResp = (HttpServletResponse) response;
+		
+		// 세션을 가져옴.
+		HttpSession session = httpReq.getSession();
+		// 세션에서 로그인 사용자 속성(signedInUser attribute) 값을 읽음.
+		Object signedInUser = session.getAttribute("signedInUser");
+		
+		if (signedInUser != null && !signedInUser.equals("")) { // 세션에 로그인 정보가 있는 경우
+			log.debug("로그인 상태: username={}", signedInUser);
+			
+			// 필터 체인의 다음 단계(다음 필터 또는 서블릿)으로 request를 전달.
+			chain.doFilter(request, response);
+			return;
+		}
+		
+		log.debug("로그아웃 상태 ---> 로그인 페이지로 이동");
+		httpResp.sendRedirect("/spring2/user/signin");
+		
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	@Override
+	public void init(FilterConfig fConfig) throws ServletException {
+		// WAS가 필터를 생성한 이후에 필터에서 초기화 작업이 필요한 코드들을 작성.
+	}
+
+}
