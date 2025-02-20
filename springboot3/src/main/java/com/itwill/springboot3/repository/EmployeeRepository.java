7211b37d40e31720d6d682eae12af16333a1c767
JakeOh <jake.oh.lecture@gmail.com>
2025-02-20 16:48:18 +0900

    homework

--- springboot3/src/main/java/com/itwill/springboot3/repository/EmployeeRepository.java
+++ springboot3/src/main/java/com/itwill/springboot3/repository/EmployeeRepository.java
@@ -31,4 +31,17 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 	// Containing을 사용하면 아규먼트의 앞/뒤에 "%"를 붙이고 like 검색을 수행.
 	// Like를 사용할 때는 아규먼트가 "%"를 포함하고 있어야 함.
 	List<Employee> findByFirstNameLike(String keyword);
+	
+	// 이름(first_name)에 포함된 단어로 검색, 단어의 대/소문자 구분 없이 검색.
+	// 이름(first_name)에 포함된 단어로 검색, 단어 대/소문자 구분 없이 검색, 정렬 순서는 이름 내림차순.
+	// 대소문자 구분없이 성(last_ame) 또는 이름(first_name)에 문자열이 포함된 직원 검색.
+	// 급여가 어떤 값을 초과하는 직원들 검색(where salary > ?).
+	// 급여가 어떤 값 미만인 직원들 검색(where salary < ?).
+	// 급여가 어떤 범위 안에 있는 직원들 검색(where salary between ? and ?).
+	// 입사날짜가 특정 날짜 이전인 직원들 검색(where hire_date < ?).
+	// 입사날짜가 특정 날짜 이후인 직원들 검색(where hire_date > ?).
+	// 입사날짜가 날짜 범위 안에 있는 직원들 검색(where hire_date between ? and ?).
+	// 부서 이름으로 직원 검색.
+	// 근무 도시 이름으로 직원 검색.
+	// 근무 국가 아이디로 직원 검색.
 }
