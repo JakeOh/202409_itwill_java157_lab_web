0a170862cf284baed78ab65dec965c215842adee
JakeOh <jake.oh.lecture@gmail.com>
2025-02-24 14:21:29 +0900

    employee list pagination

--- springboot3/src/main/java/com/itwill/springboot3/service/EmployeeService.java
+++ springboot3/src/main/java/com/itwill/springboot3/service/EmployeeService.java
@@ -2,6 +2,10 @@ package com.itwill.springboot3.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.itwill.springboot3.domain.Employee;
@@ -16,6 +20,24 @@ import lombok.extern.slf4j.Slf4j;
 public class EmployeeService {
 	
 	private final EmployeeRepository empRepo;
+	
+	// paging과 sorting이 된 객체를 리턴하는 메서드.
+	// pageNo: 페이지 번호, sort: 정렬 조건(컬럼, 방식)
+	public Page<Employee> read(int pageNo, Sort sort) {
+		log.info("read(pageNo={}, sort={})", pageNo, sort);
+		
+		// Pageable 객체 생성
+		Pageable pageable = PageRequest.of(pageNo, 10, sort);
+		
+		// JpaRepository<T, ID>.findAll(Pageable pageable) 메서드 호출
+		Page<Employee> page = empRepo.findAll(pageable);
+		log.info("previous = {}", page.hasPrevious());  // 이전 페이지가 있는 지 여부
+		log.info("next = {}", page.hasNext());  // 다음 페이지가 있는 지 여부
+		log.info("number = {}", page.getNumber());  // 현재 페이지(슬라이스) 번호
+		log.info("total pages = {}", page.getTotalPages());  // 전체 페이지 수
+		
+		return page;
+	}
 	
 	public List<Employee> read() {
 		log.info("read()");
--- springboot3/src/main/java/com/itwill/springboot3/web/EmployeeController.java
+++ springboot3/src/main/java/com/itwill/springboot3/web/EmployeeController.java
@@ -2,11 +2,14 @@ package com.itwill.springboot3.web;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.itwill.springboot3.domain.Employee;
 import com.itwill.springboot3.service.EmployeeService;
@@ -23,11 +26,12 @@ public class EmployeeController {
 	private final EmployeeService empService;
 
 	@GetMapping("/list")
-	public void list(Model model) {
-		log.info("list()");
+	public void list(@RequestParam(name = "p", defaultValue = "0") int pageNo, Model model) {
+		log.info("list(pageNo={})", pageNo);
 		
-		List<Employee> list = empService.read();
-		model.addAttribute("employees", list);
+		// 서비스 계층의 메서드를 호출해서 페이징, 정렬 처리가 된 직원 목록을 가져옴.
+		Page<Employee> list = empService.read(pageNo, Sort.by("id"));
+		model.addAttribute("page", list);
 	}
 	
 	@GetMapping("/details/{id}")
