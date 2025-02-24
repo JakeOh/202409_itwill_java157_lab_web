f0d3e1e7b11d98266fee828083af48b9588b14d7
JakeOh <jake.oh.lecture@gmail.com>
2025-02-24 12:20:53 +0900

    department details dto, view, controller, service

--- springboot3/src/main/java/com/itwill/springboot3/service/DepartmentService.java
+++ springboot3/src/main/java/com/itwill/springboot3/service/DepartmentService.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.itwill.springboot3.domain.Department;
+import com.itwill.springboot3.domain.Employee;
+import com.itwill.springboot3.dto.DepartmentDetailsDto;
 import com.itwill.springboot3.repository.DepartmentRepository;
+import com.itwill.springboot3.repository.EmployeeRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 public class DepartmentService {
 
 	private final DepartmentRepository deptRepo;
+	private final EmployeeRepository empRepo;
 	
 	public List<Department> read() {
 		log.info("read()");
@@ -23,6 +27,21 @@ public class DepartmentService {
 		List<Department> list = deptRepo.findAll();
 		
 		return list;
+	}
+	
+	public DepartmentDetailsDto read(Integer id) {
+		log.info("read(id={})", id);
+		
+		// 부서 아이디로 부서 엔터티 검색
+		Department dept = deptRepo.findById(id).orElseThrow();
+		
+		// 부서 아이디로 부서에서 근무하는 직원 목록 검색
+		List<Employee> employees = empRepo.findByDepartmentId(id);
+		
+		// 부서 상세보기 DTO 생성.
+		DepartmentDetailsDto dto = DepartmentDetailsDto.fromEntity(dept, employees);
+		
+		return dto;
 	}
 	
 }
--- springboot3/src/main/java/com/itwill/springboot3/web/DepartmentController.java
+++ springboot3/src/main/java/com/itwill/springboot3/web/DepartmentController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.itwill.springboot3.domain.Department;
+import com.itwill.springboot3.dto.DepartmentDetailsDto;
 import com.itwill.springboot3.service.DepartmentService;
 
 import lombok.RequiredArgsConstructor;
@@ -27,6 +29,16 @@ public class DepartmentController {
 		
 		List<Department> list = deptService.read();
 		model.addAttribute("departments", list);
+	}
+	
+	@GetMapping("/details/{id}")
+	public String details(@PathVariable Integer id, Model model) {
+		log.info("details(id={})", id);
+		
+		DepartmentDetailsDto dto = deptService.read(id);
+		model.addAttribute("department", dto);
+		
+		return "dept/details";
 	}
 	
 }
