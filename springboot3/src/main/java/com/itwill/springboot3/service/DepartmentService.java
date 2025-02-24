ffdf01966fa3b774a6dd01ae88213067e913478f
JakeOh <jake.oh.lecture@gmail.com>
2025-02-24 16:04:59 +0900

    department list pagination

--- springboot3/src/main/java/com/itwill/springboot3/service/DepartmentService.java
+++ springboot3/src/main/java/com/itwill/springboot3/service/DepartmentService.java
@@ -2,6 +2,10 @@ package com.itwill.springboot3.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.itwill.springboot3.domain.Department;
@@ -20,6 +24,15 @@ public class DepartmentService {
 
 	private final DepartmentRepository deptRepo;
 	private final EmployeeRepository empRepo;
+	
+	public Page<Department> read(int pageNo, Sort sort) {
+		log.info("read(pageNo={}, sort={})", pageNo, sort);
+		
+		Pageable pageable = PageRequest.of(pageNo, 5, sort);
+		Page<Department> page = deptRepo.findAll(pageable);
+		
+		return page;
+	}
 	
 	public List<Department> read() {
 		log.info("read()");
--- springboot3/src/main/java/com/itwill/springboot3/web/DepartmentController.java
+++ springboot3/src/main/java/com/itwill/springboot3/web/DepartmentController.java
@@ -1,12 +1,13 @@
 package com.itwill.springboot3.web;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.itwill.springboot3.domain.Department;
 import com.itwill.springboot3.dto.DepartmentDetailsDto;
@@ -24,11 +25,11 @@ public class DepartmentController {
 	private final DepartmentService deptService;
 	
 	@GetMapping("/list")
-	public void list(Model model) {
-		log.info("list()");
+	public void list(@RequestParam(name = "p", defaultValue = "0") int pageNo, Model model) {
+		log.info("list(pageNo={})", pageNo);
 		
-		List<Department> list = deptService.read();
-		model.addAttribute("departments", list);
+		Page<Department> list = deptService.read(pageNo, Sort.by("id"));
+		model.addAttribute("page", list);
 	}
 	
 	@GetMapping("/details/{id}")
