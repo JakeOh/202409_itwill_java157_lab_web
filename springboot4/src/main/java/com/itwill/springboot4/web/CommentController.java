bca5610b1ad2ff321c955a74ff221d870f24d87b
JakeOh <jake.oh.lecture@gmail.com>
2025-02-27 16:24:42 +0900

    comment REST controller

--- springboot4/src/main/java/com/itwill/springboot4/web/CommentController.java
+++ springboot4/src/main/java/com/itwill/springboot4/web/CommentController.java
@@ -4,13 +4,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itwill.springboot4.domain.Comment;
+import com.itwill.springboot4.dto.CommentRegisterDto;
+import com.itwill.springboot4.dto.CommentUpdateDto;
 import com.itwill.springboot4.service.CommentService;
 
 import lombok.RequiredArgsConstructor;
@@ -39,5 +45,34 @@ public class CommentController {
 		return ResponseEntity.ok(new PagedModel<>(page));
 		// JSON 직렬화의 안정성을 위해서 Page<T> 객체 대신에 PagedModel<T>을 클라이언트로 응답을 보냄.
 	}
+	
+	@PostMapping()
+	public ResponseEntity<Comment> registerComment(@RequestBody CommentRegisterDto dto) {
+		log.info("registerComment(dto={})", dto);
+		
+		Comment entity = commentService.create(dto);
+		
+		return ResponseEntity.ok(entity);
+	}
+	
+	@PutMapping("/{commentId}")
+	public ResponseEntity<Long> updateComment(@PathVariable Long commentId,
+			@RequestBody CommentUpdateDto dto) {
+		log.info("updateComment(commentId={}, dto={})", commentId, dto);
+		
+		dto.setId(commentId);
+		commentService.update(dto);
+		
+		return ResponseEntity.ok(commentId); // 업데이트한 댓글의 아이디를 응답으로 보냄.
+	}
 
+	@DeleteMapping("/{commentId}")
+	public ResponseEntity<Long> deleteComment(@PathVariable Long commentId) {
+		log.info("deleteComment(commentId={})", commentId);
+		
+		commentService.delete(commentId);
+		
+		return ResponseEntity.ok(commentId);
+	}
+	
 }
