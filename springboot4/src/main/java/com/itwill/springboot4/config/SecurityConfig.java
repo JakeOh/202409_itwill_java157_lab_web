0a1f6c4fe7be2c0d906e36d1f01db1233ffe1128
JakeOh <jake.oh.lecture@gmail.com>
2025-03-05 14:34:04 +0900

    configure SecurityFilterChain

--- springboot4/src/main/java/com/itwill/springboot4/config/SecurityConfig.java
+++ springboot4/src/main/java/com/itwill/springboot4/config/SecurityConfig.java
@@ -2,12 +2,19 @@ package com.itwill.springboot4.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 
 @Configuration
 //-> 스프링 컨테이너에서 생성하고 관리하는 설정 컴포넌트.
@@ -21,12 +28,22 @@ public class SecurityConfig {
 	// 비밀번호를 암호화하는 객체를 스프링 컨테이너가 빈으로 관리해야 함.
 	@Bean //-> 스프링 컨테이너에서 관리하는 객체(빈) -> 의존성 주입.
 	PasswordEncoder passwordEncoder() {
+		log.info("BCryptPasswordEncoder 생성");
 		return new BCryptPasswordEncoder();
 	}
 	
-	// 사용자(User) 엔터티, 리포지토리와 사용자 서비스를 구현하기 전에 테스트 용도로 사용할 코드.
+	/*
+	 * UserDetailsService:
+	 * 사용자 관리(로그인, 로그아웃, 회원가입 등)를 위한 서비스 인터페이스.
+	 * 스프링 부트 애플리케이션에서 스프링 시큐리티를 이용한 로그인/로그아웃 서비스를 구현하려면
+	 * (1) UserDetailsService 인터페이스를 구현하는 서비스 클래스와
+	 * (2) UserDetails 인터페이스를 구현하는 엔터티 클래스가 있어야 함.
+	 * 사용자(User) 엔터티, 리포지토리와 사용자 서비스를 구현하기 전에 테스트 용도로 사용할 코드.
+	 */
 	@Bean
 	UserDetailsService inMemoryUserDetailsService() {
+		log.info("InMemoryUserDetailsManager 생성");
+		
 		// 애플리케이션 동작 중에 메모리에 임시 저장할 사용자 객체를 생성
 		UserDetails user1 = User.withUsername("user1")  // 로그인 사용자 아이디
 				.password(passwordEncoder().encode("1111"))  // 암호화된 로그인 사용자 비밀번호
@@ -43,6 +60,34 @@ public class SecurityConfig {
 		
 		// User 타입 객체 3개를 메모리에서만 관리하는 사용자 상세정보 매니저 객체를 생성하고 리턴.
 		return new InMemoryUserDetailsManager(user1, user2, user3);
+	}
+	
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		log.info("SecurityFilterChain 생성");
+		
+		// CSRF 기능 비활성화:
+		http.csrf((csrf) -> csrf.disable());
+		
+		// 로그인 페이지(폼) 설정을 스프링 시큐리티에서 제공하는 기본 HTML 페이지를 사용하도록 설정
+		http.formLogin(Customizer.withDefaults());
+		
+		// 페이지 접근 권한, 인증 구성:
+		http.authorizeHttpRequests(auth -> 
+			// 모든 요청 주소에 대해서 (권한(role)에 상관없이) 아이디/비밀번호 인증을 하는 경우:
+			// auth.anyRequest().authenticated()
+		
+			// 모든 요청 주소에 대해서 "USER" 권한을 가진 사용자의 아이디/비밀번호 인증을 하는 경우:
+			// auth.anyRequest().hasRole("USER")
+		
+			auth
+			.requestMatchers("/post/create")
+			.hasRole("USER")
+			.anyRequest()
+			.permitAll()
+		);
+		
+		return http.build();
 	}
 
 }
