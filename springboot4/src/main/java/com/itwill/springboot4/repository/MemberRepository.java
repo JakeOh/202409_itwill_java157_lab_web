afd49160cd31cac62375c76b43da62dae3e2ff3f
JakeOh <jake.oh.lecture@gmail.com>
2025-03-06 17:23:06 +0900

    @EntityGraph

--- springboot4/src/main/java/com/itwill/springboot4/repository/MemberRepository.java
+++ springboot4/src/main/java/com/itwill/springboot4/repository/MemberRepository.java
@@ -1,9 +1,23 @@
 package com.itwill.springboot4.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.itwill.springboot4.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+	// @EntityGraph: static 선언된 LAZY fetch 방식을 실행 시점에(runtime) EAGER 방식으로 변환.
+	// @EntityGraph 애너테이션을 사용하지 않았을 때는 
+	// (1) select ... from members ...
+	// (2) select ... from member_roles ...
+	// 순서로 필요할 때 roles 정보를 가져옴(lazy fetch).
+	// @EntityGrapgh 애너테이션을 사용하면
+	// select ... from members m left join member_roles r on ...
+	// 와 같이 한번에 모든 정보를 가져옴(eager fetch). 
+	@EntityGraph(attributePaths = "roles")
+	Optional<Member> findByUsername(String username);
+	
 }
--- springboot4/src/test/java/com/itwill/springboot4/repository/MemberRepositoryTest.java
+++ springboot4/src/test/java/com/itwill/springboot4/repository/MemberRepositoryTest.java
@@ -2,13 +2,17 @@ package com.itwill.springboot4.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.itwill.springboot4.domain.Member;
 import com.itwill.springboot4.domain.MemberRole;
+
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -67,17 +71,35 @@ public class MemberRepositoryTest {
 		log.info("save 호출 후: entity={}, roles={}", entity, entity.getRoles());
 	}
 	
-	@Test
+//	@Test
 	public void testSave2() {
 		Member user1 = Member.builder()
-				.username("user1")
-				.password(encoder.encode("user1"))
-				.email("user1@test.com")
+				.username("user2")
+				.password(encoder.encode("user2"))
+				.email("user2@test.com")
 				.build();
-		user1.addRole(MemberRole.USER);
+		user1.addRole(MemberRole.ADMIN);
 		
 		user1 = memberRepo.save(user1);
 		log.info("save 후 user1={}, roles={}", user1, user1.getRoles());
+	}
+	
+//	@Test
+//	@Transactional
+	public void testFindAll() {
+		List<Member> members = memberRepo.findAll();
+		members.forEach(m -> {
+			System.out.println(m);
+			System.out.println("\t" + m.getRoles());
+		});
+	}
+	
+	@Test
+//	@Transactional
+	public void testFindByUsername() {
+		Member m = memberRepo.findByUsername("admin").get(); // orElseThrow()와 동일.
+		log.info("m = {}", m);
+		log.info("roles = {}", m.getRoles());
 	}
 
 }
